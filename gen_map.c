50cd10d9b65a05e4c77085f38f17e784a28d935b
maparigi <maparigi@student.42.fr>
2022-06-21 13:13:35 +0200

    prints images

--- gen_map.c
+++ gen_map.c
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 10:45:48 by maparigi          #+#    #+#             */
-/*   Updated: 2022/06/21 13:00:35 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/06/21 13:11:37 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ void	gen_map(t_coord map, t_window *win)
 {
 	int		i;
 	int		j;
-	int		xx;
-	int		yy;
+	int		xy[2];
+	int		hw[2];
 	void	*image;
-	int		xp;
-	int		yp;
 
 	i = -1;
-	xx = 0;
-	yy = 0;
+	xy[0] = 0;
+	xy[1] = 0;
 	while (map.map[++i])
 	{
 		j = -1;
 		while (map.map[i][++j] && j < map.x)
 		{
-			image = mlx_xpm_file_to_image(win->mlx, "so_long_sprites/attack-2.xpm", &xp, &yp);
-			mlx_put_image_to_window(win->mlx, win->window, image, xx, yy);
-			xx += 64;
+			image = mlx_xpm_file_to_image(win->mlx,
+					"so_long_sprites/attack-2.xpm", &hw[0], &hw[1]);
+			mlx_put_image_to_window(win->mlx, win->window, image, xy[0], xy[1]);
+			xy[0] += 64;
 		}
-		xx = 0;
-		yy += 64;
+		xy[0] = 0;
+		xy[1] += 64;
 	}
 }

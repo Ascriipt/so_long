54aa039166455e6e37d946b86f375ff6b6779ebb
Ascriipt <maparigi@student.42.fr>
2022-06-21 18:36:39 +0200

    magueule

--- gen_map.c
+++ gen_map.c
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 10:45:48 by maparigi          #+#    #+#             */
-/*   Updated: 2022/06/21 13:11:37 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/06/21 18:29:04 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,9 @@ void	gen_map(t_coord map, t_window *win)
 		while (map.map[i][++j] && j < map.x)
 		{
 			image = mlx_xpm_file_to_image(win->mlx,
-					"so_long_sprites/attack-2.xpm", &hw[0], &hw[1]);
+					"so_long_sprites/rsz_magueule.xpm", &hw[0], &hw[1]);
+			if (!image)
+				pexit_failfree("no image of this name exists.\n", map.map);
 			mlx_put_image_to_window(win->mlx, win->window, image, xy[0], xy[1]);
 			xy[0] += 64;
 		}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:34:52 by maparigi          #+#    #+#             */
-/*   Updated: 2022/06/21 13:01:55 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/06/21 18:28:56 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	my_init(t_window *win, t_data *img, t_coord map)
 		return ;
 	win->mlx = mlx_init();
 	win->window = mlx_new_window(win->mlx, ((map.x - 1) * 64),
-			((map.y - 1) * 64), "Hello world!");
+			((map.y - 1) * 64), "so_long");
 	gen_map(map, win);
 	mlx_loop(win->mlx);
 }
